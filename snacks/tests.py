b38d207e151e988e0dbc106c563c6db52f4c332c
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-24 13:01:33 -0700

    added tests for new routes

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,4 +1,4 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
 
 # Create your tests here.
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 class SnackTest(SimpleTestCase):
 
+# TEST CASE 
     def test_home_page_status(self):
         url = reverse('home')
         response = self.client.get(url)
@@ -21,6 +22,7 @@ class SnackTest(SimpleTestCase):
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'home.html')
 
+# TEST CASE ABOUT PAGE
     def test_about_page_status(self):
         url = reverse('about')
         response = self.client.get(url)
@@ -36,8 +38,24 @@ class SnackTest(SimpleTestCase):
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'about.html')
 
-
+# TEST CASE NO PAGE FOUND
     def test_no_page_found(self):
         url = 'fakey_pagey_heart/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+# TEST CASE SNACKS LIST
+    def test_snack_page_status(self):
+        url = reverse('snacks')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_snack_page_base(self):
+        url = reverse('snacks')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_snack_page_template(self):
+        url = reverse('snacks')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
