72ad8478855069e8c543d06df2ee84e04447f59b
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-24 13:00:16 -0700

    Added models to views and urls

--- snacks/views.py
+++ snacks/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from snacks.models import Snack
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -6,3 +7,6 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = 'about.html'
 
+class SnackListView(ListView):
+    template_name: 'snack_list.html'
+    model: Snack
