d50e4dacc3e6a1b20eefc7cd3cde5029950651a0
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-05-24 13:01:16 -0700

    added snack model

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class Snack(models.Model):
+    '''
+    Snack Class
+    '''
+    name = models.CharField(max_length=64)
+    purchaser = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    description = models.TextField()
